50867932e6aee85bc36c914319d9632cc63b54fa
Mark Kagan <markkagan1@gmail.com>
2023-01-27 17:52:35 +0100

    removed auth from home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,13 @@
+import { Routes, Route } from "react-router-dom";
 
-import {Routes, Route} from "react-router-dom"
-import AuthRoute from "./Firebase/AuthRoute";
-
-import './index.css'
+import "./index.css";
 import Home from "./pages/Home";
-import Login from "./pages/Login";
 
 function App() {
-
   return (
     <div className="overflow-x-hidden">
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route
-          path="/home"
-          element={
-            <AuthRoute>
-              <Home />
-            </AuthRoute>
-          }
-        />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </div>
   );
---
+++ client/src/components/DayPlanner/FocusMusic.tsx/FocusMusic.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function FocusMusic() {
+  return (
+    <div>FocusMusic</div>
+  )
+}
+
+export default FocusMusic
