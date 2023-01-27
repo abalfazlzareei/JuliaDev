27e1b55a3e3aa580fc0fa272d3d03c1c56789b84
Mark Kagan <markkagan1@gmail.com>
2023-01-27 11:27:31 +0100

    env problem solved

--- client/src/Firebase/FirebaseConfig.ts
+++ client/src/Firebase/FirebaseConfig.ts
@@ -1,23 +1,19 @@
-import dotenv from "dotenv";
-dotenv.config();
+type FirebaseConfigProps = {
+  apiKey: string
+  authDomain: string
+  databaseURL: string
+  projectId: string
+  storageBucket: string
+  messagingSenderId: string
+  appId: string
+}
 
-export const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  // measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+export const firebaseConfig: FirebaseConfigProps = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_DATABASE_URL,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_ID,
 };
-// export const firebaseConfig = {
-//   apiKey: "AIzaSyCpiWI_AUywq24fhVK_8TVe0SgTlrcOF_k",
-//   authDomain: "juliadev-b30fd.firebaseapp.com",
-//   databaseURL:
-//     "https://juliadev-b30fd-default-rtdb.europe-west1.firebasedatabase.app",
-//   projectId: "juliadev-b30fd",
-//   storageBucket: "juliadev-b30fd.appspot.com",
-//   messagingSenderId: "431389560515",
-//   appId: "1:431389560515:web:dc49bf702d4c097dc6dc76",
-// };
